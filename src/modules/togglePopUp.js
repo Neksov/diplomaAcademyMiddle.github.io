ec652164a1d3d6b1633322c89638887a00aed1e3
Nekrasov Vadim <n2v@list.ru>
2020-12-09 00:31:05 +0300

    feat: Добавил вывод модального окна

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,13 +1,35 @@
 const togglePopUp = () =>{
 
   const clubSelect = document.querySelector('.club-select'),
-        clubsList = document.querySelector('.clubs-list ul');
+        clubsList = document.querySelector('.clubs-list ul'),//кнопка выбрать клуб
+        popup = document.querySelectorAll('.popup'),
+        callbackForm = document.getElementById('callback_form'),
+        freeVisitForm = document.getElementById('free_visit_form');
+
+        clubsList.classList.add('active');
+        
+//<--реализация нажатия кнопки выбора зала-->
+        document.addEventListener('click', (event)=>{
+          if(event.target.closest('.club-select')){
+            clubsList.classList.toggle('active');
+          };
+          // console.log(event.target)
+        
+
+          if(event.target.closest('.open-popup')){
+            callbackForm.style.display = 'block';
+          }else if(event.target.closest('.callback-btn')){
+            freeVisitForm.style.display = 'block';
+          }else if(event.target.closest('.close_icon') || event.target.closest('.form-wrapper')){
+            callbackForm.style.display = 'none';
+            freeVisitForm.style.display = 'none';
+
+          }
+
+        });
 
 
-        clubSelect.addEventListener('mouseover', (event)=>{
 
-            clubsList.style.display = 'block';
-            console.log(event.target)
-    });
+
 };
 export default togglePopUp;
